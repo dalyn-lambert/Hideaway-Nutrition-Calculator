0a096ace2d00815043800db05ecf51f419f1ffac
Dalyn Lambert <jdalynlambert@gmail.com>
2024-02-23 15:00:42 -0600

    [chore] added names for breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,18 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        phone: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        laptop: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 };
