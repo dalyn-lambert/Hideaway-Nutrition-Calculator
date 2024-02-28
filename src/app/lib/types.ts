46cbdd2d0a479f35d203fa9039c324d2e37b5dc5
Dalyn Lambert <jdalynlambert@gmail.com>
2024-02-28 17:12:07 -0600

    [chore] added json data

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -1,6 +1,30 @@
+// cauliflower: {
+//   small: {
+//     calories: 90,
+//     calories_from_fat: 30,
+//     total_fat: 3.5,
+//     saturated_fat: 0.5,
+//     trans_fat: null,
+//     polyunsaturated_fat: 1.5,
+//     monounsaturated_fat: 1.5,
+//     cholesterol: 10,
+//     sodium: 55,
+//     potassium: null,
+//     total_carbohydrates: 13,
+//     dietary_fiber: 1,
+//     sugars: null,
+//     added_sugars: null,
+//     protein: 1
+//   }
 export type Category = {
   name: string;
-  options: MenuItem[];
+};
+
+export type Sizes = { small: MenuItem[]; medium: MenuItem[]; large: MenuItem[] };
+
+export type CategoryData = {
+  name: string;
+  options: Sizes;
 };
 
 export type MenuItem = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 
 import React from 'react';
-import * as data from '../../public/data.json';
-import MenuCategory from './components/MenuCategory';
-import { Category } from './lib/types';
+import * as data from '../../public/BYOPizzaData.json';
+import { CategoryData } from './lib/types';
 export default function Home() {
   const [macros, setMacros] = React.useState({ cal: 0, protein: 0, fat: 0, carb: 0 });
   const [item, setItem] = React.useState('');
-  const { crusts, sauces } = data;
-  const categories: Category[] = [
-    { name: 'crusts', options: crusts },
-    { name: 'sauces', options: sauces },
-  ];
+  const { crust, sauce } = data;
+  console.log(crust);
+  const categories: CategoryData[] = [{ name: 'crusts', options: crust }, ,];
   return (
     <main className='flex min-h-screen flex-col gap-6 items-center p-4'>
       <div>Hideaway Nutrition Calculator</div>
@@ -22,12 +19,12 @@ export default function Home() {
         <span>{macros.fat} fat</span>
         <span>{macros.carb} carb</span>
       </div>
-      {categories.map((category) => (
+      {/* {categories.map((category) => (
         <div key={category.name}>
           <span className='text-xl'>{category.name}</span>
           <MenuCategory category={category.name} items={category.options} />
         </div>
-      ))}
+      ))} */}
     </main>
   );
 }
